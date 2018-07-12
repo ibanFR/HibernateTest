ab59320b0402730161a7fc4a1b7147a8faa08399
ivan.fernandez <ivan.fernandez@empowertheuser.com>
2018-07-12 10:47:28 +0100

    added annotations to entity class

--- src/main/java/com/ibanfr/hibernate/model/User.java
+++ src/main/java/com/ibanfr/hibernate/model/User.java
@@ -2,10 +2,14 @@ package com.ibanfr.hibernate.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="user")
 public class User {
 	
 	@Id
