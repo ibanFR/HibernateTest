0b58608d4f43c6749f270eb7aeaf7fbab7cb1382
ivan.fernandez <ivan.fernandez@empowertheuser.com>
2021-02-16 16:09:45 +0100

    Code refactor for Hibernate version 5

--- src/main/java/com/ibanfr/manager/UserManager.java
+++ src/main/java/com/ibanfr/manager/UserManager.java
@@ -6,10 +6,15 @@ package com.ibanfr.manager;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 
 import com.ibanfr.hibernate.connection.HibernateUtil;
 import com.ibanfr.hibernate.model.User;
+import com.ibanfr.hibernate.model.User_;
 
 /**
  * @author IVAN
@@ -31,6 +36,7 @@ public class UserManager {
 		session.save(user);
 		
 		session.getTransaction().commit();
+		session.close();
 		
 		return user;
 	}
@@ -45,7 +51,7 @@ public class UserManager {
 			
 			User user = new User();
 
-			user.setUsername("Ivan2");
+			user.setUsername("testUser"+i);
 			user.setCreatedBy("etu");
 			user.setCreatedDate(new Date());
 
@@ -53,12 +59,52 @@ public class UserManager {
 		}
 		
 		session.getTransaction().commit();
+		session.close();
 		
 //		session.close();
 	}
 	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
 	public List<User> listAllUsers(){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		List<User> result = session.createQuery( "from User", User.class ).list();
+
+		session.getTransaction().commit();
+		session.close();
+		return result;
+	}
+	
+	/**
+	 * Find user by id using the Criteria API as a type safe alternative to JPQL.
+	 * <p>
+	 *
+	 *
+	 * 
+	 * @param userId User identifier
+	 * @return User object
+	 *
+	 * @see <a hef="http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#criteria "> http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#criteria </a>
+	 */
+	public User findUserbyId(Integer userId) {
 		
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<User> query = builder.createQuery(User.class);
+		Root<User> userRoot = query.from(User.class);
+		query.select(userRoot);
+		query.where(builder.equal(userRoot.get(User_.USER_ID), userId));
+		User u = session.createQuery(query).getSingleResult();
+		
+		session.getTransaction().commit();
+		session.close();
+		return u;
+		
 	}
 }
--- src/test/java/com/ibanfr/hibernate/manager/UserManagerTest.java
+++ src/test/java/com/ibanfr/hibernate/manager/UserManagerTest.java
@@ -1,15 +1,44 @@
 package com.ibanfr.hibernate.manager;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 import com.ibanfr.hibernate.model.User;
 import com.ibanfr.manager.UserManager;
 
-public class UserManagerTest {
 
-	public static void main(String[] args) {
+public class UserManagerTest {
+	
+	@Test
+	public void testListUsers() {
 		
 		UserManager manager = new UserManager();
-		User u = manager.createUser("Ivan2", "ibanFR2");
-		
-		System.out.println(u.toString());
+		List<User> users = manager.listAllUsers();
+		for (User user : users) {
+			System.out.println(user.toString());
+		}
 	}
+	@Test
+	public void testInsertUsers() {
+		
+			UserManager manager = new UserManager();
+			manager.createUsers(2);
+				
+		
+	}
+	@Test
+	public void testFindUserById() {
+		
+			UserManager manager = new UserManager();
+			User u = manager.findUserbyId(1);
+			System.out.println(u.toString());
+			assertNotNull(u);
+			assertEquals(1, u.getUserId());
+		
+	}
+
 }
